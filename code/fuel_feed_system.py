765ae561c0967042f0145d798dafaf14a51d4bdb
Haoyu Miao <47076619+zjumhy97@users.noreply.github.com>
2023-06-17 20:10:15 +0800

    add fuel_feed_system.py

---
+++ code/fuel_feed_system.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+__all__ = ['FuelFeedSystem']
+
+class FuelFeedSystem:
+    def __init__(self, aircraft_weight:float, ) -> None:
+        self.aircraft_weight = aircraft_weight
+        # initialize the parameter, 
+
+
+    def calculate_center_of_gravity(self, weight:np.ndarray) -> np.ndarray:
+        pass
